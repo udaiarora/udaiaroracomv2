a9ef3d6cf5211924cce51797f5ef0547c84d07d5
Udai Arora <rocketmailudai@gmail.com>
2014-07-28 00:33:14 -0700

    Everything in place. Minor changes required

--- public/main.js
+++ public/main.js
@@ -10,35 +10,28 @@ $(window).load(function(){
 
 
 $(document).ready(function(){
+	 TagCanvas.Start('tagCanvas','tags',{
+            textColour: '#aaa',
+            outlineColour: '#555',
+            reverse: true,
+            depth: 0.5,
+            noselect: true,
+            padding: 100,
+            initial: [0.05,0.1],
+            maxSpeed: 0.1
+          });
+
+
+
 	(function($){
 		$(window).load(function(){
-
-			// $(".main-content").mCustomScrollbar({
-			// 	scrollButtons:{
-			// 		enable:true
-			// 	},
-			// 	scrollInertia: 500,
-			// 	theme: "minimal",
-			// 	callbacks:{
-			// 		whileScrolling:function(){
-			// 			var distFromTop=-this.mcs.top;
-			// 			$('.blurred').css('opacity', distFromTop/200);
-			// 			if( distFromTop <= 300 ) {
-
-			// 				$('#head-text').css({top:distFromTop+200});
-
-			// 			} else {
-
-			// 				$('#head-text').css({top:500});
-			// 			}
-			// 		}
-			// 	}
-			// });
 		});
 	})(jQuery);
+
+
 	$(document).scroll(function() {
 		var distFromTop=$(this).scrollTop();
-		$('.blurred').css('opacity', distFromTop/250);
+		$('.blurred').css('opacity', distFromTop/150);
 		if( distFromTop <= 300 ) {
 
 			$('#head-text').css({top:distFromTop+200});
@@ -48,6 +41,40 @@ $(document).ready(function(){
 			$('#head-text').css({top:500});
 		}
 	});
+
+
+	var anim_angle=360;
+	var isRunning=false;
+
+	$("#change-text, .spin-text").on("click", function(ev) {
+    	if (!isRunning){
+    		isRunning=true;
+    		$("#change-text").removeClass("breath");
+	    	$("#change-text").css({'-webkit-transform':'rotate('+anim_angle+'deg)','-moz-transform':'rotate('+anim_angle+'deg)', '-ms-transform':'rotate('+anim_angle+'deg)','-o-transform':'rotate('+anim_angle+'deg)', '-webkit-transition-duration':'500ms', '-moz-transition-duration':'500ms', '-o-transition-duration':'500ms', '-ms-transition-duration':'500ms'});
+	    	anim_angle+=360;
+	    	$(".spin-text").find(".visible").fadeOut(200, function(){
+
+
+			if (0==$(".spin-text").find(".visible").next().length) 
+			{
+				$(".spin-text").find(".visible").siblings().first().removeClass("hidden").addClass("visible").removeAttr("style");
+			}
+			else 
+			{
+				$(".spin-text").find(".visible").next().removeClass("hidden").addClass("visible").removeAttr("style");
+			}
+
+		    $(this).addClass("hidden").removeClass("visible");
+		    	
+	    	});
+	    	setTimeout(function()
+	    	{
+	    		isRunning=false;
+	    		$("#change-text").addClass("breath");
+	    	}, 500);
+		}
+		    	
+    })
 
 })
 
