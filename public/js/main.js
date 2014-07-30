da4bbc3a150b73356a6eb6df698fc571fc31cea8
Udai Arora <rocketmailudai@gmail.com>
2014-07-29 19:22:44 -0700

    final touches

---
+++ public/js/main.js
@@ -0,0 +1,101 @@
+
+var anim_angle=360;
+var isRunning=false;
+
+
+$(function() {
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 500);
+				return false;
+			}
+		}
+	});
+});
+
+$(window).load(function(){
+	$(".foo").fadeOut(500, function(){
+		// $("html").niceScroll({scrollspeed : "100", autohidemode : false});
+	});
+	$(".main-content").css({"display":"block"});
+});
+
+
+
+$(document).ready(function(){
+	TagCanvas.Start('tagCanvas','tags',{
+		textColour: '#eee',
+		textHeight: 10,
+		noSelect: true,
+		outlineColour: '#555',
+		initial: [0.05,0.15],
+		maxSpeed: 0.1
+	});
+
+
+
+	(function($){
+		$(window).load(function(){
+		});
+	})(jQuery);
+
+
+	$(document).scroll(function() {
+		var distFromTop=$(this).scrollTop();
+		$('.blurred').css('opacity', distFromTop/150);
+		if( distFromTop <= 300 ) {
+
+			$('#head-text').css({top:distFromTop+200});
+
+		} else {
+
+			$('#head-text').css({top:500});
+		}
+	});
+
+
+
+	$("#change-text, .spin-text").on("click", function(ev) {
+		$("#change-text").removeClass("breath");
+		setTimeout(function()
+			{$("#change-text").css({'-webkit-transform':'rotate('+anim_angle+'deg)','-moz-transform':'rotate('+anim_angle+'deg)', '-ms-transform':'rotate('+anim_angle+'deg)','-o-transform':'rotate('+anim_angle+'deg)', '-webkit-transition-duration':'500ms', '-moz-transition-duration':'500ms', '-o-transition-duration':'500ms', '-ms-transition-duration':'500ms'});
+		}
+		,10)
+
+
+		setTimeout(function()
+			{if (!isRunning){
+				isRunning=true;
+
+				anim_angle+=360;
+				$(".spin-text").find(".visible").fadeOut(200, function(){
+					if (0==$(".spin-text").find(".visible").next().length) 
+					{
+						$(".spin-text").find(".visible").siblings().first().removeClass("hidden").addClass("visible").removeAttr("style");
+					}
+					else 
+					{
+						$(".spin-text").find(".visible").next().removeClass("hidden").addClass("visible").removeAttr("style");
+					}
+
+					$(this).addClass("hidden").removeClass("visible");
+
+				});
+			}
+			
+			isRunning=false;
+			$("#change-text").addClass("breath");
+		}
+		, 500);
+		
+
+	});
+
+});
+
+
